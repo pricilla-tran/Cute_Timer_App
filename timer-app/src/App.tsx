ef13b4687167d8dcab9796715b84a05f8fbfd214
Pricilla Tran <71370147+pricilla-tran@users.noreply.github.com>
2025-08-25 22:34:33 +1000

    Basic Format

--- timer-app/src/App.tsx
+++ timer-app/src/App.tsx
@@ -1,24 +1,48 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [timeLeft, setTimeLeft] = useState(25*60)
+  const [isRunning, setIsRunning] = useState(false)
+
+  useEffect( () => {
+    let timer: NodeJS.Timeout;
+    // if timer is running and there is time left
+    if (isRunning && timeLeft > 0){
+      // set timer
+      timer = setInterval( () => {
+        // take off a second
+        setTimeLeft(prev => prev - 1)
+      }, 1000) // signifies 1000 milliseconds which = 1 second
+    }
+    // Else return cleared timer
+    return() => clearInterval(timer)
+
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{position: 'relative'}}>
+      <div>
+        <button className="closeButton">
+          Close
+        </button>
+      </div>
+      <div className="home-content">
+        <div className="home-controls">
+          <button className="image-button">
+            Work
+          </button>
+          <button className="image-button">
+            Break
+          </button>
+        </div>
+        <p>You can do it!</p>
+        <h1 className="home-timer">25:00</h1>
+        <button className="home-button">
+            Break
+          </button>
+      </div>
     </div>
   );
 }
